073bdffd5960f76d9d7e11e6e6ede9609782a704
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-05-14 17:23:21 -0400

    Added urldecode and urlencode

--- General.h
+++ General.h
@@ -1,4 +1,9 @@
 #pragma once
 
+#include <Arduino.h>
+
 // following define disables interrupts but enables them does not matter how function has returned
 #define PAUSE_INTERRUPTS struct _t { _t() { noInterrupts(); } ~_t() { interrupts(); } } _
+
+String urldecode(String str);
+String urlencode(String str);
